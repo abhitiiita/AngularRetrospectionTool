e03499ef0aa004945b8b422116bf46f302532688
Abhinav <abhit.iiita@gmail.com>
2015-08-26 09:58:23 +0530

    resolve passport issue with password select false

--- config/passport-local.js
+++ config/passport-local.js
@@ -58,21 +58,20 @@ module.exports = function(passport){
         passwordField: 'password',
         passReqToCallback: true //allows us to pass back the entire request to the callback
         }, function(req, email, password, done){
+            console.log(email);
+            console.log(password);
         // find a user whose email is the same as the forms email
         // we are checking to see if the user trying to login already exists
-        User.findOne({ 'email' :  email }, function(err, user) {
+        User.findOne({ 'email' :  email },'+password', function(err, user) {
             // if there are any errors, return the error before anything else
             if (err)
                 return done(err);
-
             // if no user is found, return the message
             if (!user)
                 return done(null, false, 'No user found.'); // req.flash is the way to set flashdata using connect-flash
-
             // if the user is found but the password is wrong
             if (!user.validPassword(password))
                 return done(null, false, 'Oops! Wrong password.'); // create the loginMessage and save it to session as flashdata
-
             // all is well, return successful user
             return done(null, user);
         });
